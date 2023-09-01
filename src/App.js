c9f13ace24927c02a33f1936454b53541dec07ec
azrulwm <azrulazmi.wm@gmail.com>
2023-09-01 09:20:36 +0800

    Adding sidebar and header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <Sidebar />
       </Router>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>Header</h1>
+      <h1>open/close sidebar</h1>
+    </div>
+  );
+};
+
+export default Header;
